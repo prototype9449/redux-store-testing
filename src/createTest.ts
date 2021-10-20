232cb7649702cddc312c3cd189e1f003391084f1
prototype9449 <7286647+prototype9449@users.noreply.github.com>
2021-10-21 00:05:02 +0300

    rename to StoreTester

--- src/createTest.ts
+++ src/createTest.ts
@@ -1,3 +1,3 @@
-import {StoreTester, StoreTesterParams} from './store-tester';
+import {StoreTester, StoreTesterParams} from './StoreTester';
 
 export const createTest = <T>(options: StoreTesterParams<T>): StoreTester<T> => new StoreTester<T>(options)
